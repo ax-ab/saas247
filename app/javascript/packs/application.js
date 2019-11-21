cdc6ca037073b7afa39f51011b3ad9387a4b345d
Lars Houbak <lars.houbak@gmail.com>
2019-11-21 18:12:29 +0100

    card with calendar on dashboard

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,9 @@
 import "bootstrap";
 import { leftNavInteractivity } from "./leftnav.js";
 import { tableStyleOverride } from "./table.js";
+import { initCalendar } from "../plugins/init-calendar.js";
 
-tableStyleOverride();
+
+/*tableStyleOverride();*/
 leftNavInteractivity();
+initCalendar();
---
+++ app/javascript/plugins/init-calendar.js
@@ -0,0 +1,28 @@
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+
+import '@fullcalendar/core/main.css';
+import '@fullcalendar/daygrid/main.css';
+//import '@fullcalendar/timegrid/main.css';
+//import '@fullcalendar/list/main.css';
+
+function initCalendar(){
+  document.addEventListener('DOMContentLoaded', function() {
+    console.log('yay')
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new Calendar(calendarEl, {
+      plugins: [ dayGridPlugin ],
+      events: '/api/v1/events',
+      eventClick: function(info) {
+        alert(info.event);
+      },
+      firstDay: 1
+    });
+
+    calendar.render();
+  });
+}
+
+export { initCalendar }
