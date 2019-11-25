ff9b6761a3c87a92a753eba9370873fb87c3e285
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2019-11-25 17:36:29 +0100

    started landing page html + css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:index]
+  layout 'landing-page'
 
-  def home
+  def index
   end
 end
