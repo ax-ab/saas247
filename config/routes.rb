74e9704eec0a10de38b86554bdf270ec6b82b4de
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2019-11-27 16:28:01 +0100

    Added 404, 422 and 500 error pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'errors/show'
   devise_for :users
   root to: 'pages#index'
 
@@ -11,6 +12,14 @@ Rails.application.routes.draw do
   get 'usage/new/:user_id', to: 'license_usages#new', as: 'new_survey'
   get 'usage/survey/:user_id', to: 'license_usages#user_survey', as: 'user_survey'
   get 'usage/survey', to: 'license_usages#all_survey', as: 'all_survey'
+
+  # %w( 404 422 500 ).each do |code|
+  #   get code, controller: :error, action: :show, code: code
+  # end
+
+  get '/404', to: 'errors#not_found'
+  get '/422', to: 'errors#unacceptable'
+  get '/500', to: 'errors#internal_error'
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
